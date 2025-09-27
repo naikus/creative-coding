3f1e8a99ea34e49b539fa403468d8dab33340dcc
Aniket Naik <aniketn3@gmail.com>
2025-09-27 17:31:27 +0530

    Done with sketch

--- src/sketch_01_3.js
+++ src/sketch_01_3.js
@@ -12,11 +12,24 @@ const settings = {
     // dimensions: [1024, 1024]
     bleed: 10,
     // export settings
-    file: "sketch_01.js"
+    file: "sketch_01_3.js"
+    // ,animate: true
   },
 
+  colors = [
+    "#ffd16fcc",
+    "#ade359bb",
+    "#72ebf988",
+    "#e668a777",
+    "#00b6d599",
+    "#00b6d599",
+    "#f8557199",
+    "#f8ede8b6",
+    "rgba(255, 112, 56, 0.66)"
+  ],
+
   options = {
-    fillStyle: "rgba(21, 31, 41, 1)",
+    fillStyle: "rgb(26, 27, 38)",
     strokeStyle: "rgb(245, 245, 245)"
   };
 
@@ -32,25 +45,24 @@ function setup(sc) {
   context.fillRect(0, 0, width, height);
 }
 
-function sketch() {
+function sketch(sc) {
+  // setup(sc);
   /**
    * @param {SketchContext} sc
    */
   return function(sc) {
     setup(sc);
     const {context: ctx,  width, height} = sc;
-
     ctx.fillStyle = "rgba(217, 223, 221, 1)";
     ctx.strokeStyle = "rgba(212, 228, 228, 1)";
 
     const cx = width * 0.5,
         cy = height * 0.5,
-
         // create a horizontal strip-like rectangle
         w = width * 0.1,
         h = height * 0.01;
 
-    const slices = 24,
+    const slices = 48,
       slice = math.degToRad(360 / slices),
       radius = width * 0.2;
 
@@ -71,9 +83,10 @@ function sketch() {
       }
       ctx.translate(x, y);
       ctx.rotate(angle);
-      ctx.scale(random.range(.2, .5), random.range(0.1, 2));
+      ctx.scale(random.range(0.2, 0.5), random.range(0.2, 2));5
       ctx.beginPath();
-      ctx.rect(w * random.range(0.1 , 1.5), -h * 0.5, w, h);
+      ctx.rect(-w * random.range(0.2, 2), -h * 0.5, w * random.range(1, 2), h);
+      ctx.fillStyle = colors[random.rangeFloor(0, colors.length)];
       ctx.fill();
       ctx.restore();
 
@@ -82,9 +95,11 @@ function sketch() {
       ctx.save();
       ctx.translate(cx, cy);
       ctx.rotate(angle);
-      ctx.lineWidth = random.range(3, 7);
+      ctx.lineWidth = random.range(3, 20);
+      ctx.strokeStyle = "rgb(16, 150, 212)";
       ctx.beginPath();
-      ctx.arc(0, 0, radius * random.range(0.7, 1.2), slice * random.range(1, -8), slice);
+      ctx.arc(0, 0, radius * random.range(0.2, 1.3), slice * random.range(0.3, -8), slice * random.range(0.3, 8));
+      ctx.strokeStyle = colors[random.rangeFloor(0, colors.length)];
       ctx.stroke();
       // ctx.fill();
       ctx.restore();
